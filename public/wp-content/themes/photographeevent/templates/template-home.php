5c354f4a39746592359e742f3684a50cb8c4b202
Audrey <appere.audrey29@gmail.com>
2023-06-01 21:55:29 +0200

    hero ok

--- public/wp-content/themes/photographeevent/templates/template-home.php
+++ public/wp-content/themes/photographeevent/templates/template-home.php
@@ -5,27 +5,27 @@
  */
 ?>
 
-<?php get_header(); ?>
-
-<h1 class="nom-site"><?php the_title() ?></h1>
-<!-- récupère contenu  -->
-<?php the_content() ?>
-
 <!-- hero -->
 
+<?php get_header(); ?>
+<div class="hero-container">
+    <h1 class="nom-site"><?php the_title() ?></h1>
 
-
-<div class="accueil_aleatoire_photo">
     <?php query_posts(
         array(
             'post_type' => 'photo',
             'showposts' => 1,
             'orderby' => 'rand',
+            'terms' => 'paysage',
         )
     ); ?>
     <?php if (have_posts()) :
         while (have_posts()) :
             the_post(); ?>
-            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title_attribute(); ?>" <?php
-                                                                                            endwhile;
-                                                                                        endif; ?> <?php get_footer(); ?>
+            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title_attribute(); ?>"> <?php
+                                                                                                endwhile;
+                                                                                            endif; ?>
+</div>
+<!-- galerie -->
+
+<?php get_footer(); ?>
